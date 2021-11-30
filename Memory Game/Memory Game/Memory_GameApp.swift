95ffd1793bb8795ffc1976d3de2349c1863788a9
brunovgois <brunovgois@gmail.com>
2021-11-30 01:51:01 -0300

    Initial commit

---
+++ Memory Game/Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Bruno Gois on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
